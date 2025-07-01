1d01d31bfab203c74a3b71b319f155ba2db48698
Timbaleek <timbaleek@hotmail.com>
2025-07-01 13:39:25 +0200

    amogus

---
+++ src/lib/Titlebar.svelte
@@ -0,0 +1,12 @@
+<h1 class="title">amogussy</h1>
+
+<style>
+    h1 {
+        color: rgb(144, 0, 255);
+        text-align: center;
+    }
+
+    .title {
+        font-family: "Courier New", Courier, monospace;
+    }
+</style>
---
+++ src/routes/amogus/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Titlebar from "$lib/Titlebar.svelte";
+</script>
+
+<h1>amogus</h1>
+<Titlebar></Titlebar>
+<Titlebar></Titlebar>
+
+<style>
+    h1 {
+        color: red;
+        text-align: center;
+    }
+
+    body {
+        background-color: #000;
+        color: #fff;
+        font-family: Arial, sans-serif;
+    }
+</style>
