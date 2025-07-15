9568e6f2809754202ff4ff8840ef46f293589e36
Timbaleek <timbaleek@hotmail.com>
2025-07-15 15:25:05 +0200

    about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,22 +4,36 @@
 
     import ArticleList from "$lib/ArticleList.svelte";
 
+    //import all images from $lib/assets/about
+    import laraPB from "$lib/assets/about/laraPB.jpeg";
+    import melaniePB from "$lib/assets/about/melaniePB.jpeg";
+    import timPB from "$lib/assets/about/timPB.jpeg";
+
     let people = [
         {
-            title: "Max Mustermann",
-            subtitle: "Schauspieler",
-            image: "path/to/image.jpg",
+            title: "Lara Eggert",
+            subtitle: "Präsidentin, Organisation",
+            image: laraPB,
             description:
-                "Max Mustermann ist ein erfahrener Schauspieler und Gründungsmitglied des Vereins.",
-            date: "Seit 2020 aktiv",
+                "Theater ist mein Ding, Pink mein Element und ich werde bestimmt mal an einem Lachflash sterben. Ich bin die, die Chaos tanzt, Glitzer streut, nie leise denkt und trotzdem nie das Ziel aus den Augen verliert - höchstens mal das Skript.",
+            date: "Seit März 2025",
         },
         {
-            title: "Erika Musterfrau",
-            subtitle: "Regisseurin",
-            image: "path/to/image.jpg",
+            title: "Melanie Dobler",
+            subtitle: "Finanzielles, Tickets",
+            image: melaniePB,
             description:
-                "Erika Musterfrau ist eine talentierte Regisseurin und Mitglied des Vereins.",
-            date: "Seit 2020 aktiv",
+                "Ich kann besser Text uswendig lernen als formulieren und ich spreche am liebsten mit anderen, wenn es in einem Skrip steht. Ausserdem ist mein Lieblingsdino der Ankylosaurier, nur so nebenbei.",
+            date: "Seit März 2025",
+        },
+
+        {
+            title: "Tim Appel",
+            subtitle: "Kreatives, Design, Programmierung",
+            image: timPB,
+            description:
+                "Ich spiele seit meiner Kindheit Theater und liebe es, Geschichten zu erfinden. Wenn ich auf der Bühne stehe, fühle ich mich, als hätte ich das Leben meines Charakters erlebt.",
+            date: "Seit März 2025",
         },
     ];
 </script>
