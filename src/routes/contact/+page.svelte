1b7a86ca138638f6871e89248695d5ad74faf5d8
Timbaleek <timbaleek@hotmail.de>
2026-07-15 11:33:16 +0200

    added link to gemeindefinder

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import BaseWrapper from "$lib/BaseWrapper.svelte";
-    import MailingListForm from "$lib/MailingListForm.svelte";
+    //import MailingListForm from "$lib/MailingListForm.svelte";
     import MailToVorstand from "$lib/MailToVorstand.svelte";
-    import Titlebar from "$lib/Titlebar.svelte";
     import InstaQR from "$lib/assets/contact/bruggetrolle_qr.png";
-    import { expoIn } from "svelte/easing";
+    import gemeindefinder from "$lib/assets/contact/gemeindefinde-logo-icon.webp";
+    import bruggWappen from "$lib/assets/contact/CHE_Brugg_COA.svg.webp";
 </script>
 
 <BaseWrapper>
@@ -40,8 +40,31 @@
                 </div>
             </div>
         </div>
-    </div></BaseWrapper
->
+        <div>
+            Wir sind ein eingetragener Verein der Stadt Brugg: 
+            <div class="columns is-variable is-8">
+                <div class="column">
+                    <figure class="image is-128x128">
+                        <a href="https://gemeinde-schweiz.ch/kanton-aargau/brugg/" 
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <img src={gemeindefinder} alt="Gemeindefinder Logo" />
+                        </a>
+                    </figure>
+                </div>
+                <div class="column">
+                    <figure class="image is-128x128">
+                        <a href="https://www.brugg.ch/themen-a-z/vereine/details.html/33/association/123"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <img src={bruggWappen} alt="Stadt Brugg Logo" />
+                        </a>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</BaseWrapper>
 
 <style>
 </style>
